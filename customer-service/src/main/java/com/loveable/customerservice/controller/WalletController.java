7820937dc694d10c3e11e47b902b3cb648950a1a
Mensaloveable <abdullahi.adedayo@gmail.com>
2023-03-05 13:30:56 +0100

    Add Eureka Service

---
+++ customer-service/src/main/java/com/loveable/customerservice/controller/WalletController.java
@@ -0,0 +1,25 @@
+package com.loveable.customerservice.controller;
+
+import com.loveable.customerservice.service.WalletService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/wallet")
+public class WalletController {
+    private final WalletService walletService;
+
+    @PostMapping
+    public ResponseEntity<BillingResponse> fundWallet(@RequestBody BigDecimal amount){
+        BillingResponse billingResponse = walletService.fundWallet(amount);
+        return new ResponseEntity<>(billingResponse, HttpStatus.OK);
+    }
+}
